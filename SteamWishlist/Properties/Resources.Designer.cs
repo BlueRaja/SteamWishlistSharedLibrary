512a1f9b87b808b3a3b87e3f36ccf8be3c2d1951
BlueRaja <BlueRaja.admin@gmail.com>
2016-01-16 09:57:45 -0600

    Added crappy icon and updated Window title

--- SteamWishlist/Properties/Resources.Designer.cs
+++ SteamWishlist/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace SteamWishlist.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Wishlist {
+            get {
+                object obj = ResourceManager.GetObject("Wishlist", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
